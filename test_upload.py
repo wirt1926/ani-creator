5f76a7c951f22fae6caa190e869e516db79321d6
Pimkey <adka94@op.pl>
2018-05-12 07:17:45 +0000

    test - corrected bucket properties

--- test_upload.py
+++ test_upload.py
@@ -1,11 +1,11 @@
 import boto3
 s3 = boto3.resource('s3')
-bucket = s3.Bucket('kkanclerz-zid')
-my_file = open('to_be_uploaded.txt', 'w+')
+bucket = s3.Bucket('203828-ada2')
+my_file = open('/home/ec2-user/Files/test4.txt', 'w+')
 my_file.write('Here will be my test content, isint it cool?')
 my_file.close()
 
 bucket.put_object(
-  Key='omega/gamma/test.txt',
-  Body=open('to_be_uploaded.txt', 'rb')
+  Key='MyTxtFiles/test4.txt',
+  Body=open('/home/ec2-user/Files/test4.txt', 'rb')
 )
